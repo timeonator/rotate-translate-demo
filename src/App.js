a9f6ebc57a2f81dd7f7ea3171454cbca736b6bef
Robert Bruce Gifford <timeonator@gmail.com>
2021-02-22 15:12:01 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Canvas from "./canvas";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Solar System Demo</h1>
+      <Canvas />
+    </div>
+  );
+}
